0ed0d6272d355f04a470ee78012bfa008e2d0c83
KarLOL82 <klinfeldt@gmail.com>
2022-12-08 20:52:00 -0800

    expanded model properties

--- models/User.js
+++ models/User.js
@@ -5,12 +5,15 @@ const userSchema = new schema(
         username: {
             type: String,
             required: true,
+            unique: true, 
+            trim: true,
 
         },
 
         email: {
             type: String,
             required: true,
+            unique: true,
         },
     }
 )
