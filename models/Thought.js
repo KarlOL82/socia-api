02f2c44b33802ed8df0cac43f253123ab92e7a5b
KarLOL82 <klinfeldt@gmail.com>
2022-12-10 21:50:54 -0800

    finishing model schemas, need to build virtuals

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { schema, model } = require('mongoose');
+
+const thoughtSchema = new schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 280
+
+        },
+
+        createdAt: {
+            type: Date,
+            required: true,
+            unique: true,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        }
+
+        reactions: [
+            {
+                type: Schema.types.objectId,
+                ref: 'Thought'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.types.objectId,
+                ref: 'User'
+            }
+        ]
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
